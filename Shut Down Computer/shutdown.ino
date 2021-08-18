7dbebf5cb216b141191440b770701061103b1ef8
Ahmad Mujahid <ahmad.kirghizia@gmail.com>
2021-08-18 14:31:58 +0700

    remove powershell command as it is unnecessary

--- Shut Down Computer/shutdown.ino
+++ Shut Down Computer/shutdown.ino
@@ -7,13 +7,7 @@ void setup() {
   DigiKeyboard.sendKeyStroke(KEY_R | 0, MOD_GUI_LEFT);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("powershell");
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.delay(1000);
-  DigiKeyboard.print("shutdown /s");
+  DigiKeyboard.print("shutdonw /s");
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
